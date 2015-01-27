8a9008f497667de9e6cc19e7c8e683f15b4f2e76
Nihlus <jarl.gullberg@gmail.com>
2015-01-27 17:01:53 +0100

    Added .gitignore.

--- Launchpad_Launcher/bin/Debug/update.bat
+++
@@ -1,2 +0,0 @@
-timeout 3 & xcopy /s /y "C:\Users\Jarl\AppData\Local\Temp\Launchpad.exe" "F:\ProjectAzeroth\Launcher\src\Launchpad_Launcher\launchpad_Launcher\bin\Debug\Launchpad.exe" && del "C:\Users\Jarl\AppData\Local\Temp\Launchpad.exe"
-start Launchpad.exe
