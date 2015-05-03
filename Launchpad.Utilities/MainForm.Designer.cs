d770d25343d1cc5503e4436fd885ea1e1b8138a4
Nihlus <jarl.gullberg@gmail.com>
2015-05-03 15:54:07 +0200

    Oops. Missed it. Added Launchpad.Utilities to the main solution.

---
+++ Launchpad.Utilities/MainForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Launchpad.Utilities
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.generateManifest_button = new System.Windows.Forms.Button();
+            this.utilTools_progressBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileProgress_label = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.currentFile_label = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.generateSingle_button = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.md5Result_Textbox = new System.Windows.Forms.TextBox();
+            this.backgroundWorker_manifestGenerator = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // generateManifest_button
+            // 
+            this.generateManifest_button.Location = new System.Drawing.Point(13, 13);
+            this.generateManifest_button.Name = "generateManifest_button";
+            this.generateManifest_button.Size = new System.Drawing.Size(249, 35);
+            this.generateManifest_button.TabIndex = 0;
+            this.generateManifest_button.Text = "Generate Launcher Manifest";
+            this.generateManifest_button.UseVisualStyleBackColor = true;
+            this.generateManifest_button.Click += new System.EventHandler(this.generateManifest_button_Click);
+            // 
+            // utilTools_progressBar
+            // 
+            this.utilTools_progressBar.Location = new System.Drawing.Point(13, 352);
+            this.utilTools_progressBar.Name = "utilTools_progressBar";
+            this.utilTools_progressBar.Size = new System.Drawing.Size(249, 23);
+            this.utilTools_progressBar.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Completed files: ";
+            // 
+            // fileProgress_label
+            // 
+            this.fileProgress_label.AutoSize = true;
+            this.fileProgress_label.Location = new System.Drawing.Point(102, 101);
+            this.fileProgress_label.Name = "fileProgress_label";
+            this.fileProgress_label.Size = new System.Drawing.Size(24, 13);
+            this.fileProgress_label.TabIndex = 4;
+            this.fileProgress_label.Text = "0/0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Current file:";
+            // 
+            // currentFile_label
+            // 
+            this.currentFile_label.AutoSize = true;
+            this.currentFile_label.Location = new System.Drawing.Point(12, 139);
+            this.currentFile_label.Name = "currentFile_label";
+            this.currentFile_label.Size = new System.Drawing.Size(0, 13);
+            this.currentFile_label.TabIndex = 6;
+            // 
+            // generateSingle_button
+            // 
+            this.generateSingle_button.Location = new System.Drawing.Point(12, 54);
+            this.generateSingle_button.Name = "generateSingle_button";
+            this.generateSingle_button.Size = new System.Drawing.Size(250, 35);
+            this.generateSingle_button.TabIndex = 7;
+            this.generateSingle_button.Text = "Generate Single MD5 Hash";
+            this.generateSingle_button.UseVisualStyleBackColor = true;
+            this.generateSingle_button.Click += new System.EventHandler(this.generateSingle_button_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "MD5:";
+            // 
+            // md5Result_Textbox
+            // 
+            this.md5Result_Textbox.Location = new System.Drawing.Point(50, 169);
+            this.md5Result_Textbox.Name = "md5Result_Textbox";
+            this.md5Result_Textbox.ReadOnly = true;
+            this.md5Result_Textbox.Size = new System.Drawing.Size(212, 20);
+            this.md5Result_Textbox.TabIndex = 9;
+            // 
+            // backgroundWorker_manifestGenerator
+            // 
+            this.backgroundWorker_manifestGenerator.WorkerReportsProgress = true;
+            this.backgroundWorker_manifestGenerator.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_manifestGenerator_DoWork);
+            this.backgroundWorker_manifestGenerator.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_manifestGenerator_ProgressChanged);
+            this.backgroundWorker_manifestGenerator.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_manifestGenerator_RunWorkerCompleted);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 387);
+            this.Controls.Add(this.md5Result_Textbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.generateSingle_button);
+            this.Controls.Add(this.currentFile_label);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fileProgress_label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.utilTools_progressBar);
+            this.Controls.Add(this.generateManifest_button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Launchpad - Utilites";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button generateManifest_button;
+        private System.Windows.Forms.ProgressBar utilTools_progressBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label fileProgress_label;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label currentFile_label;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button generateSingle_button;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox md5Result_Textbox;
+        private System.ComponentModel.BackgroundWorker backgroundWorker_manifestGenerator;
+    }
+}
+
---
+++ Launchpad.Utilities/MainForm.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace Launchpad.Utilities
+{
+    public partial class MainForm : Form
+    {
+        bool bIsGeneratingManifest = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        
+        /// <summary>
+        /// Computes the MD5 hash for the specified file stream.
+        /// </summary>
+        /// <param name="fileStream"></param>
+        /// <returns>The MD5 hash.</returns>
+        private static string GetFileHash(Stream fileStream)
+        {
+            try
+            {
+                using (MD5 md5 = MD5.Create())
+                {
+                    //we got a valid file, calculate the MD5 and return it
+                    string hash = BitConverter.ToString(md5.ComputeHash(fileStream)).Replace("-", "");
+                    return hash;
+                }
+            }
+            catch (IOException iex)
+            {
+                Console.WriteLine("IOException in GetFileHash(): " + iex.Message);                
+                return null;
+            }
+        }
+
+        private void generateManifest_button_Click(object sender, EventArgs e)
+        {
+            if ( bIsGeneratingManifest == false)
+            {
+                DialogResult folderSelectionResult = folderBrowserDialog1.ShowDialog();
+                backgroundWorker_manifestGenerator.RunWorkerAsync(folderSelectionResult);
+            }            
+        }
+
+        private static string GenerateSingleHash(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                Stream selectedFile = null;
+                try
+                {
+                    selectedFile = File.OpenRead(filePath);
+
+                    string hash = GetFileHash(selectedFile);
+
+                    return hash;
+                }
+                catch (IOException iex)
+                {
+                    Console.WriteLine("IOException in GenerateSingle(): " + iex.Message);
+                    return null;
+                }
+                catch (UnauthorizedAccessException uex)
+                {
+                    Console.WriteLine("UnauthorizedAccessException in GenerateSingle(): " + uex.Message);
+                    return null;
+                }
+                finally
+                {
+                    if (selectedFile != null)
+                    {
+                        selectedFile.Close();
+                    }
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void generateSingle_button_Click(object sender, EventArgs e)
+        {
+            string filePath = openFileDialog1.FileName;
+            string hash = GenerateSingleHash(filePath);
+
+            if (!String.IsNullOrEmpty(hash))
+            {
+                md5Result_Textbox.Text = hash;
+            }                       
+        }
+
+        private void backgroundWorker_manifestGenerator_DoWork(object sender, DoWorkEventArgs e)
+        {
+            bIsGeneratingManifest = true;
+            DialogResult result = (DialogResult)e.Argument;
+
+            if (result == DialogResult.OK)
+            {
+                string parentDirectory = Directory.GetParent(folderBrowserDialog1.SelectedPath).ToString();
+                string manifestPath = String.Format(@"{0}{1}LauncherManifest.txt", parentDirectory, Path.DirectorySeparatorChar);
+                string manifestChecksumPath = String.Format(@"{0}{1}LauncherManifest.checksum", parentDirectory, Path.DirectorySeparatorChar);
+
+
+                if (File.Exists(manifestPath))
+                {
+                    //create a new empty file and close it (effectively deleting the old manifest)
+                    File.Create(manifestPath).Close();
+                }
+
+                TextWriter tw = new StreamWriter(manifestPath);
+
+                string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*", SearchOption.AllDirectories);                
+                int completedFiles = 0;
+                
+                IEnumerable<string> enumeratedFiles = Directory
+                                                    .EnumerateFiles(folderBrowserDialog1.SelectedPath, "*", SearchOption.AllDirectories);
+
+                foreach (string file in enumeratedFiles)
+                {
+                    if (file != null)
+                    {
+                        FileStream fileStream = File.OpenRead(file);
+                        var skipDirectory = folderBrowserDialog1.SelectedPath;
+
+                        int fileAmount = files.Length; 
+                        string currentFile = file.Substring(skipDirectory.Length);
+
+                        //get file size on disk
+                        FileInfo Info = new FileInfo(file);
+                        string fileSize = Info.Length.ToString();
+                        
+                        string manifestLine = String.Format(@"{0}:{1}:{2}", file.Substring(skipDirectory.Length), GetFileHash(fileStream), fileSize);
+                        fileStream.Close();
+
+                        completedFiles++;
+                        
+
+                        tw.WriteLine(manifestLine);
+                        backgroundWorker_manifestGenerator.ReportProgress(completedFiles, new Tuple<int, int, string>(fileAmount, completedFiles, currentFile));
+                    }
+                }
+                tw.Close();
+
+
+                //create a manifest checksum file.
+                string manifestHash = GetFileHash(File.OpenRead(manifestPath));
+                
+                FileStream checksumStream = File.Create(manifestChecksumPath);
+
+                TextWriter tw2 = new StreamWriter(checksumStream);
+                tw2.WriteLine(manifestHash);
+                tw2.Close();
+            }
+        }
+
+        private void backgroundWorker_manifestGenerator_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Tuple<int, int, string> state = (Tuple<int, int, string>)e.UserState;
+
+            utilTools_progressBar.Maximum = state.Item1;
+
+            fileProgress_label.Text = String.Format(@"{0}/{1}", state.Item2, state.Item1);
+            fileProgress_label.Refresh();
+
+            currentFile_label.Text = state.Item3;
+            currentFile_label.Refresh();
+
+            utilTools_progressBar.Increment(1);
+        }
+
+        private void backgroundWorker_manifestGenerator_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            bIsGeneratingManifest = false;            
+        }
+    }
+}
