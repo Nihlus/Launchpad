9a70c9fabb06f499e4c630c208ec247637fc3056
Jarl Gullberg <jarl.gullberg@gmail.com>
2016-03-15 15:12:13 +0100

    Made the permissions a little more reasonable. Will change to respect umask in the future.

--- Launchpad.Launcher/Handlers/UnixHandler.cs
+++ Launchpad.Launcher/Handlers/UnixHandler.cs
@@ -32,7 +32,7 @@ namespace Launchpad.Launcher.Handlers
 	{
 		/// <summary>
 		/// Sets the execute bit on target file. Note that this replaces all previous
-		/// permissions on the file, resulting in RWXRWXR-- permissions.
+		/// permissions on the file, resulting in rwx-r-x-r-- permissions.
 		/// </summary>
 		/// <returns><c>true</c>, if operation succeeded, <c>false</c> otherwise.</returns>
 		/// <param name="fileName">File name.</param>
@@ -40,7 +40,7 @@ namespace Launchpad.Launcher.Handlers
 		{
 			try
 			{
-				Syscall.chmod(fileName, FilePermissions.S_IRWXU | FilePermissions.S_IRWXG | FilePermissions.S_IROTH);
+				Syscall.chmod(fileName, FilePermissions.S_IRWXU | FilePermissions.S_IRGRP | FilePermissions.S_IXGRP | FilePermissions.S_IROTH);
 				return true;
 			}
 			catch (ApplicationException aex)
