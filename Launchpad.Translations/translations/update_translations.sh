fd3ae547ae688e549c0de11f64d2cba26fbdbf21
Jarl Gullberg <jarl.gullberg@gmail.com>
2016-03-22 21:43:10 +0100

    Updated translation update script.

--- Launchpad.Translations/translations/update_translations.sh
+++ Launchpad.Translations/translations/update_translations.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+wget https://crowdin.com/download/project/launchpad.zip launchpad.zip
+unzip launchpad.zip
+
 for D in `find . -type d`
 do
 	LOCALE_NAME=${D/./}
@@ -9,4 +13,11 @@ do
 	then
 		cp "$D/messages.po" "../$LOCALE_NAME.po"
 	fi	
+done
+
+rm launchpad.zip
+rm index.html
+for D in `find . -type d`
+do
+	rm -r $D
 done
