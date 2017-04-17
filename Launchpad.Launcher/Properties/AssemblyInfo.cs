041176611a13acab5c0cafc88443a13293a5038f
Jarl Gullberg <jarl.gullberg@gmail.com>
2017-04-17 21:02:39 +0200

    Released new version, and bumped version twice.

--- Launchpad.Launcher/Properties/AssemblyInfo.cs
+++ Launchpad.Launcher/Properties/AssemblyInfo.cs
@@ -53,8 +53,8 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.2.*")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.1.*")]
+[assembly: AssemblyFileVersion("0.4.1.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-GB")]
 
 // Log4Net XML activation
