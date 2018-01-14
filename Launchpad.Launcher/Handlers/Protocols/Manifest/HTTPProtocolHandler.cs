a7f6761d347dfdecc8dd0d3bc25f6738cb6cab9a
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-01-14 21:54:59 +0100

    Correct file headers.

--- Launchpad.Launcher/Handlers/Protocols/Manifest/HTTPProtocolHandler.cs
+++ Launchpad.Launcher/Handlers/Protocols/Manifest/HTTPProtocolHandler.cs
@@ -4,7 +4,7 @@
 //  Author:
 //       Jarl Gullberg <jarl.gullberg@gmail.com>
 //
-//  Copyright (c) 2016 Jarl Gullberg
+//  Copyright (c) 2017 Jarl Gullberg
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -13,11 +13,12 @@
 //
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See themanifest
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
 //
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 using System;
 using System.Drawing;
--- Launchpad.Launcher/Handlers/Protocols/PatchProtocolHandler.cs
+++ Launchpad.Launcher/Handlers/Protocols/PatchProtocolHandler.cs
@@ -4,7 +4,7 @@
 //  Author:
 //       Jarl Gullberg <jarl.gullberg@gmail.com>
 //
-//  Copyright (c) 2016 Jarl Gullberg
+//  Copyright (c) 2017 Jarl Gullberg
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -18,6 +18,7 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 using System;
 using System.Drawing;
