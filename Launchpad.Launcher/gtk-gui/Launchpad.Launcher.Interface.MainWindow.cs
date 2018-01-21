53ef129e5be440e35b4c0608f515e8a1f5e8def7
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-01-21 19:09:20 +0100

    Reorganize the UI file.

--- Launchpad.Launcher/gtk-gui/Launchpad.Launcher.Interface.MainWindow.cs
+++ Launchpad.Launcher/gtk-gui/Launchpad.Launcher.Interface.MainWindow.cs
@@ -32,6 +32,8 @@ namespace Launchpad.Launcher.Interface
 	{
 		private UIManager UIManager;
 
+		private ActionGroup MainActionGroup;
+
 		private Action MenuAction;
 
 		private Action RepairGameAction;
@@ -73,54 +75,48 @@ namespace Launchpad.Launcher.Interface
 		{
 			Gui.Initialize(this);
 
+			CreateWidgets();
+			BuildHierarchy();
+			InitializeWidgets();
+			BindEvents();
+
+			this.Child?.ShowAll();
+			Show();
+		}
+
+		private void CreateWidgets()
+		{
 			// Widget Launchpad.Launcher.Interface.MainWindow
 			this.UIManager = new UIManager();
-			var mainActionGroup = new ActionGroup("Default");
+
+			this.MainActionGroup = new ActionGroup("Default");
 
 			this.MenuAction = new Action("MenuAction", LocalizationCatalog.GetString("Menu"), null, null)
 			{
 				ShortLabel = LocalizationCatalog.GetString("Menu")
 			};
-			mainActionGroup.Add(this.MenuAction, null);
 
-			this.RepairGameAction = new Action(
+			this.RepairGameAction = new Action
+			(
 				"repairGameAction",
 				LocalizationCatalog.GetString("Repair Game"),
 				LocalizationCatalog.GetString("Starts a repair process for the installed game."),
-				"gtk-refresh")
+				"gtk-refresh"
+			)
 			{
 				ShortLabel = LocalizationCatalog.GetString("Repair Game")
 			};
-			mainActionGroup.Add(this.RepairGameAction, null);
 
-			this.ReinstallGameAction = new Action(
+			this.ReinstallGameAction = new Action
+			(
 				"reinstallGameAction",
 				LocalizationCatalog.GetString("Reinstall Game"),
 				LocalizationCatalog.GetString("Reinstalls the installed game."),
-				"gtk-refresh")
+				"gtk-refresh"
+			)
 			{
 				ShortLabel = LocalizationCatalog.GetString("Reinstall Game")
 			};
-			mainActionGroup.Add(this.ReinstallGameAction, null);
-
-			this.UIManager.InsertActionGroup(mainActionGroup, 0);
-			AddAccelGroup(this.UIManager.AccelGroup);
-			this.Name = "Launchpad.Launcher.Interface.MainWindow";
-			this.Title = LocalizationCatalog.GetString("Launchpad - {0}");
-
-			if (PlatformHelpers.IsRunningOnUnix())
-			{
-				this.Icon = Pixbuf.LoadFromResource("Launchpad.Launcher.Resources.RocketIcon.ico");
-			}
-			else
-			{
-				this.Icon = Pixbuf.LoadFromResource("Launchpad.Launcher.Resources.RocketIcon_Grey.ico");
-			}
-
-			this.WindowPosition = (WindowPosition)4;
-			this.DefaultWidth = 745;
-			this.DefaultHeight = 415;
-			this.Resizable = false;
 
 			// Container child Launchpad.Launcher.Interface.MainWindow.Gtk.Container+ContainerChild
 			this.VBox1 = new VBox
@@ -129,7 +125,6 @@ namespace Launchpad.Launcher.Interface
 				Spacing = 6
 			};
 
-			// Container child VBox1.Gtk.Box+BoxChild
 			this.UIManager.AddUiFromString
 			(
 				"<ui>" +
@@ -143,13 +138,6 @@ namespace Launchpad.Launcher.Interface
 				"</ui>"
 			);
 			this.MainMenuBar = (MenuBar)this.UIManager.GetWidget("/MainMenuBar");
-			this.MainMenuBar.Name = "MainMenuBar";
-			this.VBox1.Add(this.MainMenuBar);
-
-			var w2 = (Box.BoxChild)this.VBox1[this.MainMenuBar];
-			w2.Position = 0;
-			w2.Expand = false;
-			w2.Fill = false;
 
 			// Container child VBox1.Gtk.Box+BoxChild
 			this.HBox2 = new HBox
@@ -180,16 +168,6 @@ namespace Launchpad.Launcher.Interface
 				Name = "browserWindow",
 				ShadowType = ShadowType.In
 			};
-			this.Alignment2.Add(this.BrowserWindow);
-			this.BrowserContainer.Add(this.Alignment2);
-
-			var w4 = (Box.BoxChild)this.BrowserContainer[this.Alignment2];
-			w4.Position = 0;
-			this.HBox2.Add(this.BrowserContainer);
-
-			var w5 = (Box.BoxChild)this.HBox2[this.BrowserContainer];
-			w5.Position = 0;
-			w5.Expand = false;
 
 			// Container child HBox2.Gtk.Box+BoxChild
 			this.Alignment5 = new Alignment(0.5F, 0.5F, 1F, 1F)
@@ -205,64 +183,6 @@ namespace Launchpad.Launcher.Interface
 				Name = "gameBanner",
 				Pixbuf = Pixbuf.LoadFromResource("Launchpad.Launcher.Resources.RocketIcon.ico")
 			};
-			this.Alignment5.Add(this.GameBanner);
-			this.HBox2.Add(this.Alignment5);
-
-			var w7 = (Box.BoxChild)this.HBox2[this.Alignment5];
-			w7.Position = 1;
-			this.VBox1.Add(this.HBox2);
-
-			var w8 = (Box.BoxChild)this.VBox1[this.HBox2];
-			w8.Position = 1;
-			w8.Expand = false;
-			w8.Fill = false;
-
-			// Container child VBox1.Gtk.Box+BoxChild
-			this.Alignment1 = new Alignment(0.5F, 0.5F, 1F, 1F)
-			{
-				Name = "alignment1",
-				LeftPadding = 6
-			};
-
-			// Container child alignment1.Gtk.Container+ContainerChild
-			this.IndicatorLabel = new Label
-			{
-				Name = "IndicatorLabel",
-				Xalign = 0F,
-				LabelProp = LocalizationCatalog.GetString("Idle")
-			};
-			this.Alignment1.Add(this.IndicatorLabel);
-			this.VBox1.Add(this.Alignment1);
-
-			var w10 = (Box.BoxChild)this.VBox1[this.Alignment1];
-			w10.Position = 2;
-			w10.Expand = false;
-			w10.Fill = false;
-
-			// Container child VBox1.Gtk.Box+BoxChild
-			this.HBox3 = new HBox
-			{
-				Name = "HBox3",
-				Spacing = 6,
-				BorderWidth = 4
-			};
-
-			// Container child HBox3.Gtk.Box+BoxChild
-			this.Alignment4 = new Alignment(0.5F, 0.5F, 1F, 1F)
-			{
-				Name = "alignment4"
-			};
-
-			// Container child alignment4.Gtk.Container+ContainerChild
-			this.MainProgressBar = new ProgressBar
-			{
-				Name = "mainProgressBar"
-			};
-			this.Alignment4.Add(this.MainProgressBar);
-			this.HBox3.Add(this.Alignment4);
-
-			var w12 = (Box.BoxChild)this.HBox3[this.Alignment4];
-			w12.Position = 0;
 
 			// Container child HBox3.Gtk.Box+BoxChild
 			this.HBox4 = new HBox
@@ -288,29 +208,147 @@ namespace Launchpad.Launcher.Interface
 				UseUnderline = true,
 				Label = LocalizationCatalog.GetString("Inactive")
 			};
+
+			// Container child VBox1.Gtk.Box+BoxChild
+			this.Alignment1 = new Alignment(0.5F, 0.5F, 1F, 1F)
+			{
+				Name = "alignment1",
+				LeftPadding = 6
+			};
+
+			// Container child alignment1.Gtk.Container+ContainerChild
+			this.IndicatorLabel = new Label
+			{
+				Name = "IndicatorLabel",
+				Xalign = 0F,
+				LabelProp = LocalizationCatalog.GetString("Idle")
+			};
+
+			// Container child VBox1.Gtk.Box+BoxChild
+			this.HBox3 = new HBox
+			{
+				Name = "HBox3",
+				Spacing = 6,
+				BorderWidth = 4
+			};
+
+			// Container child HBox3.Gtk.Box+BoxChild
+			this.Alignment4 = new Alignment(0.5F, 0.5F, 1F, 1F)
+			{
+				Name = "alignment4"
+			};
+
+			// Container child alignment4.Gtk.Container+ContainerChild
+			this.MainProgressBar = new ProgressBar
+			{
+				Name = "mainProgressBar"
+			};
+		}
+
+		private void BuildHierarchy()
+		{
+			this.MainActionGroup.Add(this.MenuAction, null);
+			this.MainActionGroup.Add(this.RepairGameAction, null);
+			this.MainActionGroup.Add(this.ReinstallGameAction, null);
+
+			this.UIManager.InsertActionGroup(this.MainActionGroup, 0);
+			AddAccelGroup(this.UIManager.AccelGroup);
+
+			this.VBox1.Add(this.MainMenuBar);
+
+			this.Alignment2.Add(this.BrowserWindow);
+			this.BrowserContainer.Add(this.Alignment2);
+
+			this.HBox2.Add(this.BrowserContainer);
+
+			this.Alignment5.Add(this.GameBanner);
+			this.HBox2.Add(this.Alignment5);
+
+			this.VBox1.Add(this.HBox2);
+
+			this.Alignment1.Add(this.IndicatorLabel);
+			this.VBox1.Add(this.Alignment1);
+
+			this.Alignment4.Add(this.MainProgressBar);
+			this.HBox3.Add(this.Alignment4);
+
 			this.Alignment3.Add(this.PrimaryButton);
 			this.HBox4.Add(this.Alignment3);
 
+			this.HBox3.Add(this.HBox4);
+
+			this.VBox1.Add(this.HBox3);
+
+			Add(this.VBox1);
+		}
+
+		private void InitializeWidgets()
+		{
+			this.Name = "Launchpad.Launcher.Interface.MainWindow";
+			this.Title = LocalizationCatalog.GetString("Launchpad - {0}");
+
+			if (PlatformHelpers.IsRunningOnUnix())
+			{
+				this.Icon = Pixbuf.LoadFromResource("Launchpad.Launcher.Resources.RocketIcon.ico");
+			}
+			else
+			{
+				this.Icon = Pixbuf.LoadFromResource("Launchpad.Launcher.Resources.RocketIcon_Grey.ico");
+			}
+
+			this.WindowPosition = (WindowPosition)4;
+			this.DefaultWidth = 745;
+			this.DefaultHeight = 415;
+			this.Resizable = false;
+
+			this.MainMenuBar.Name = "MainMenuBar";
+
+			var w2 = (Box.BoxChild)this.VBox1[this.MainMenuBar];
+			w2.Position = 0;
+			w2.Expand = false;
+			w2.Fill = false;
+
+			var w4 = (Box.BoxChild)this.BrowserContainer[this.Alignment2];
+			w4.Position = 0;
+
+			var w5 = (Box.BoxChild)this.HBox2[this.BrowserContainer];
+			w5.Position = 0;
+			w5.Expand = false;
+
+			var w7 = (Box.BoxChild)this.HBox2[this.Alignment5];
+			w7.Position = 1;
+
+			var w8 = (Box.BoxChild)this.VBox1[this.HBox2];
+			w8.Position = 1;
+			w8.Expand = false;
+			w8.Fill = false;
+
+			var w10 = (Box.BoxChild)this.VBox1[this.Alignment1];
+			w10.Position = 2;
+			w10.Expand = false;
+			w10.Fill = false;
+
+			var w12 = (Box.BoxChild)this.HBox3[this.Alignment4];
+			w12.Position = 0;
+
 			var w14 = (Box.BoxChild)this.HBox4[this.Alignment3];
 			w14.Position = 0;
-			this.HBox3.Add(this.HBox4);
 
 			var w15 = (Box.BoxChild)this.HBox3[this.HBox4];
 			w15.Position = 1;
 			w15.Expand = false;
 			w15.Fill = false;
-			this.VBox1.Add(this.HBox3);
 
 			var w16 = (Box.BoxChild)this.VBox1[this.HBox3];
 			w16.Position = 3;
 			w16.Expand = true;
 			w16.Fill = false;
 
-			Add(this.VBox1);
-			this.Child?.ShowAll();
-
 			this.PrimaryButton.HasDefault = true;
-			Show();
+		}
+
+		private void BindEvents()
+		{
 			this.DeleteEvent += OnDeleteEvent;
 			this.RepairGameAction.Activated += OnRepairGameActionActivated;
 			this.ReinstallGameAction.Activated += OnReinstallGameActionActivated;
