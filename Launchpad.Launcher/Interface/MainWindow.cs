7ae652ec528d4415f8ab4ed947052e770e6ae3c0
Jarl Gullberg <jarl.gullberg@gmail.com>
2020-07-17 14:02:22 +0200

    Remove unused usings.

--- Launchpad.Launcher/Interface/MainWindow.cs
+++ Launchpad.Launcher/Interface/MainWindow.cs
@@ -21,7 +21,6 @@
 //
 
 using System;
-using System.Diagnostics;
 using System.Text.RegularExpressions;
 using Gdk;
 using GLib;
--- Launchpad.Launcher/Utility/ResourceManager.cs
+++ Launchpad.Launcher/Utility/ResourceManager.cs
@@ -20,7 +20,6 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-using System.Reflection;
 using Gdk;
 
 namespace Launchpad.Launcher.Utility
